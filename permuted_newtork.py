a657f767ef038d91e56588b04ff0e41cf4f85ea8
Factral <Factral@users.noreply.github.com>
2023-06-29 15:15:40 -0500

    added learn_offset

--- permuted_newtork.py
+++ permuted_newtork.py
@@ -10,7 +10,6 @@ class PermutedNetwork(nn.Module):
         self.conv_offset_1 = nn.Conv2d(1,3*3*2, kernel_size=3, padding=0, bias=True)
         self.conv1 = DeformConv2d(1, 16, kernel_size=3, stride=1, padding=0)
 
-
         self.conv_offset_2 = nn.Conv2d(16,3*3*2, kernel_size=3, padding=0, bias=True)
         self.conv2 = DeformConv2d(16, 32, kernel_size=3, stride=1, padding=0)
         
@@ -37,22 +36,22 @@ class PermutedNetwork(nn.Module):
     def forward(self, x):
 
 
-        offset = torch.relu(self.conv_offset_1(x))
+        offset = self.conv_offset_1(x)
         x = torch.relu(self.conv1(x, offset))
 
-        offset = torch.relu(self.conv_offset_2(x))
+        offset = self.conv_offset_2(x)
         x = torch.relu(self.conv2(x, offset))
 
-        offset = torch.relu(self.conv_offset_3(x))
-        x = torch.relu(self.conv3(x, offset))
+        offset = self.conv_offset_3(x)
+        x = self.conv3(x, offset)
 
-        offset = torch.relu(self.conv_offset_4(x))
+        offset = self.conv_offset_4(x)
         x = torch.relu(self.conv4(x, offset))
 
-        offset = torch.relu(self.conv_offset_5(x))
+        offset = self.conv_offset_5(x)
         x = torch.relu(self.conv5(x, offset))
 
-        offset = torch.relu(self.conv_offset_6(x))
+        offset = self.conv_offset_6(x)
         x = torch.relu(self.conv6(x, offset))
 
         x = x.flatten(start_dim=1)
@@ -86,17 +85,17 @@ class PermutedNetwork_2(nn.Module):
     def forward(self, x):
 
 
-        offset = torch.relu(self.conv_offset_1(x))
+        offset = self.conv_offset_1(x)
         x = torch.relu(self.conv1(x, offset)) # 33x33
 
         x = self.pool(x) # 15x15
 
-        offset = torch.relu(self.conv_offset_2(x))
+        offset = self.conv_offset_2(x)
         x = torch.relu(self.conv2(x, offset)) # 14x14
 
         x = self.pool(x) # 7x7
 
-        offset = torch.relu( self.conv_offset_3(x) )
+        offset = self.conv_offset_3(x) 
         x = torch.relu( self.conv3(x, offset) )
 
         x = self.gap(x)
@@ -106,3 +105,43 @@ class PermutedNetwork_2(nn.Module):
         return x
         
 
+class NormalNetwork(nn.Module):
+    def __init__(self):
+        super(NormalNetwork, self).__init__()
+
+        self.conv1 = nn.Conv2d(1, 16, kernel_size=3, stride=1, padding=1)
+
+        self.conv2 = nn.Conv2d(16, 32, kernel_size=3, stride=1, padding=1)
+        
+        self.conv3 = nn.Conv2d(32, 16, kernel_size=3, stride=1, padding=1)
+        
+
+        self.pool = nn.MaxPool2d(2, 2)  
+        self.batchnorm = nn.BatchNorm2d(16)
+        self.gap = nn.AdaptiveAvgPool2d( (1, 1) )
+
+        self.fc = nn.Linear(16, 10)
+
+
+    def forward(self, x):
+
+
+
+        x = torch.relu(self.conv1(x)) # 33x33
+
+        x = self.pool(x) # 15x15
+
+
+        x = torch.relu(self.conv2(x)) # 14x14
+
+        x = self.pool(x) # 7x7
+
+
+        x = torch.relu( self.conv3(x) )
+
+        x = self.gap(x)
+        x = x.flatten(start_dim=1)
+        x = self.fc(x)
+
+        return x
+        
